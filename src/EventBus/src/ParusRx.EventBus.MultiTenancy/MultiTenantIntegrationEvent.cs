ce17646eb7c1542b399e3f66e41ada7bd8a67599
Alexander Bocharov <alex.bocharov@outlook.com>
2023-07-14 16:50:59 +0300

    feat: add EventBus

---
+++ src/EventBus/src/ParusRx.EventBus.MultiTenancy/MultiTenantIntegrationEvent.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) The Parus RX Authors. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+namespace ParusRx.EventBus.MultiTenancy;
+
+/// <summary>
+/// Represents an integration event with a tenant identifier.
+/// </summary>
+/// <param name="TenantId">The unique identifier of the tenant.</param>
+public record MultiTenantIntegrationEvent(string TenantId) : IntegrationEvent;
